493fbf1eb9cf3c922206401453397836ebb261e1
Leo Dion <leogdion@brightdigit.com>
2022-06-29 14:56:10 -0400

    Initial Commit

---
+++ SwiftTubeApp/SwiftTubeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTubeAppApp.swift
+//  SwiftTubeApp
+//
+//  Created by Leo Dion on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTubeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
